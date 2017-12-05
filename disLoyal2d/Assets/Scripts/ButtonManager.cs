f98887d25d2e3cac73274d6a8f59f36a4205996a
Floepst <34098897+Floepst@users.noreply.github.com>
2017-12-05 12:15:19 +0100

    Base Disloyal
    Database implementation

---
+++ disLoyal2d/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    // Function for loading scenes
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
