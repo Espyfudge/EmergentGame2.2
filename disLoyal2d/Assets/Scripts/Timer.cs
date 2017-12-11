0ff289157afc6a2e807c883915ebc6494178f6a4
Espyfudge <rutger101_@hotmail.nl>
2017-12-11 14:23:53 +0100

    Title + GameScreen
    Title screen is not yet animated for lack of individual images. Game
    Screen has buttons that are not functional, timer loops because no
    condition set for when it reaches 0 yet.

---
+++ disLoyal2d/Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	private Image imFill;
+	private float acFill = 1;
+	// Use this for initialization
+	void Start () {
+		imFill = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		acFill -= Time.deltaTime / 10;
+		
+		imFill.fillAmount = acFill;
+
+		if ( imFill.fillAmount <= 0.0f ) {
+			imFill.fillAmount = acFill = 1.0f;
+
+			// what happens when time is up
+		}
+	}
+}
---
+++ disLoyal2d/Assets/Scripts/dbManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.Sqlite;
+using System;
+using System.Data;
+
+public class dbManager : MonoBehaviour {
+    private string constr;
+    private IDbConnection dbc;
+    private IDbCommand dbcm;
+    private IDataReader dbr;
+
+    // Use this for initialization
+    void Start () {
+        constr = "URI=file:" + Application.dataPath + "/Database/disloyalDB.db";
+        dbc = new SqliteConnection(constr);
+        dbc.Open();
+        dbcm = dbc.CreateCommand();
+        dbcm.CommandText = "SELECT * FROM Scenes";
+        dbr = dbcm.ExecuteReader();
+
+        while (dbr.Read())
+        {
+            string text = dbr.GetString(0);
+            string ans1 = dbr.GetString(1);
+            string ans2 = dbr.GetString(2);
+            string ans3 = dbr.GetString(3);
+            string ans4 = dbr.GetString(4);
+            int ans1to = dbr.GetInt16(5);
+            int ans2to = dbr.GetInt16(6);
+            int ans3to = dbr.GetInt16(7);
+            int ans4to = dbr.GetInt16(8);
+            int ans1p = dbr.GetInt16(9);
+            int ans2p = dbr.GetInt16(10);
+            int ans3p = dbr.GetInt16(11);
+            int ans4p = dbr.GetInt16(12);
+
+
+            Debug.Log("text =" + text + " ans1 = " + ans1 + " ans2 = " + ans2 + " ans3 = " + ans3 + " ans4 = " + ans4 + " ans1to = " + ans1to + " ans2to = " + ans2to + " ans3to = " + ans3to + " ans4to = " + ans4to + " ans1p = " + ans1p + " ans2p = " + ans2p + " ans3p = " + ans3p + " ans4p = " + ans4p);
+        }
+        dbr.Close();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
